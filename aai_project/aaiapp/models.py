33c04465d94cdd431094db894641e7f7ecbc5df7
Simran <makhijas180@gmail.com>
2022-09-25 21:56:43 +0530

    Completed models.py

--- aai_project/aaiapp/models.py
+++ aai_project/aaiapp/models.py
@@ -1,3 +1,12 @@
+import email
+from email import message
 from django.db import models
 
-# Create your models here.
+class AiModel(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField()
+    phone = models.IntegerField()
+    message = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name

595b29482084bbf2a1eafd1a8d82f68063d64157
Simran <makhijas180@gmail.com>
2022-09-25 22:11:41 +0530

    Modified views.py

--- aai_project/aaiapp/views.py
+++ aai_project/aaiapp/views.py
@@ -1,4 +1,17 @@
+import email
+from email import message
 from django.shortcuts import render
 
+from aai_project.aaiapp.models import AiModel
+
 def home(request):
-    return render(request, "home.html")
+    if request.method=="POST":
+        na = request.POST.get("name")
+        em = request.POST.get("email")
+        ph = request.POST.get("phone")
+        m = request.POST.get("message")
+        data = AiModel(name=na, email=em, phone=ph, message=m)
+        data.save()
+        return render(request, "home.html")
+    else:
+        return render(request, "home.html")
